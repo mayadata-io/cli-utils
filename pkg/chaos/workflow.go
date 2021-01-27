02454321248b9260817ac84dc9beff671548b85d
Raj Babu Das <raj.das@mayadata.io>
2021-01-28 01:02:39 +0530

    changing custom workflow struct to go struct
    Signed-off-by: Raj Babu Das <raj.das@mayadata.io>

--- pkg/chaos/workflow.go
+++ pkg/chaos/workflow.go
@@ -6,66 +6,71 @@ import (
 	ymlparser "gopkg.in/yaml.v2"
 	"log"
 	"net/url"
+	v1alpha1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 )
 
-type WorkflowYAML struct {
-	APIVersion string `yaml:"apiVersion"`
-	Kind       string `yaml:"kind"`
-	Metadata   struct {
-		Name      string            `yaml:"name"`
-		Namespace string            `yaml:"namespace"`
-		Labels    map[string]string `yaml:"labels"`
-	} `yaml:"metadata"`
-	Spec struct {
-		Arguments       Arguments `yaml:"arguments"`
-		Entrypoint      string    `yaml:"entrypoint"`
-		SecurityContext struct {
-			RunAsNonRoot bool `yaml:"runAsNonRoot"`
-			RunAsUser    int  `yaml:"runAsUser"`
-		} `yaml:"securityContext"`
-		ServiceAccountName string     `yaml:"serviceAccountName"`
-		Templates          []Template `yaml:"templates"`
-	} `yaml:"spec"`
-}
-
-type Input struct {
-	Artifacts []Artifact `yaml:"artifacts"`
-}
-
-type Artifact struct {
-	Name string `yaml:"name"`
-	Path string `yaml:"path"`
-	Raw  Raw    `yaml:"raw"`
-}
-
-type Raw struct {
-	Data string `yaml:"data"`
-}
-
-type Template struct {
-	Name      string   `yaml:"name"`
-	Steps     [][]Step `yaml:"steps,omitempty"`
-	Input     Input    `yaml:"inputs,omitempty"`
-	Container struct {
-		Args    []string `yaml:"args"`
-		Command []string `yaml:"command"`
-		Image   string   `yaml:"image"`
-	} `yaml:"container,omitempty"`
-}
-
-type Step struct {
-	Name     string `yaml:"name"`
-	Template string `yaml:"template"`
-}
-
-type Arguments struct {
-	Parameters []Parameter `yaml:"parameters"`
-}
-
-type Parameter struct {
-	Name  string `yaml:"name"`
-	Value string `yaml:"value"`
-}
+//type WorkflowYAML struct {
+//	metav1.TypeMeta   `json:",inline"`
+//
+//	APIVersion string `yaml:"apiVersion"`
+//	Kind       string `yaml:"kind"`
+//	Metadata   struct {
+//		Name      string            `yaml:"name"`
+//		Namespace string            `yaml:"namespace"`
+//		Labels    map[string]string `yaml:"labels"`
+//	} `yaml:"metadata"`
+//	Spec struct {
+//		Arguments       Arguments `yaml:"arguments"`
+//		Entrypoint      string    `yaml:"entrypoint"`
+//		SecurityContext struct {
+//			RunAsNonRoot bool `yaml:"runAsNonRoot"`
+//			RunAsUser    int  `yaml:"runAsUser"`
+//		} `yaml:"securityContext"`
+//		ServiceAccountName string     `yaml:"serviceAccountName"`
+//		Templates          []Template `yaml:"templates"`
+//	} `yaml:"spec"`
+//}
+//
+//type Input struct {
+//	Artifacts []Artifact `yaml:"artifacts"`
+//}
+//
+//type Artifact struct {
+//	Name string `yaml:"name"`
+//	Path string `yaml:"path"`
+//	Raw  Raw    `yaml:"raw"`
+//}
+//
+//type Raw struct {
+//	Data string `yaml:"data"`
+//}
+//
+//type Template struct {
+//	Name      string   `yaml:"name"`
+//	Steps     [][]Step `yaml:"steps,omitempty"`
+//	Input     Input    `yaml:"inputs,omitempty"`
+//	Container struct {
+//		Args    []string `yaml:"args"`
+//		Command []string `yaml:"command"`
+//		Image   string   `yaml:"image"`
+//	} `yaml:"container,omitempty"`
+//}
+//
+//type Step struct {
+//	Name     string `yaml:"name"`
+//	Template string `yaml:"template"`
+//}
+//
+//type Arguments struct {
+//	Parameters []Parameter `yaml:"parameters"`
+//}
+//
+//type Parameter struct {
+//	Name  string `yaml:"name"`
+//	Value string `yaml:"value"`
+//}
 
 type ListPkgData struct {
 	Data struct {
@@ -143,33 +148,39 @@ func GetYamlData(inputs GenerateWorkflowInputs) (YAMLData, error) {
 
 func GenerateWorkflow(wf_inputs GenerateWorkflowInputs) ([]byte, error) {
 
-	var yaml WorkflowYAML
+	var yaml v1alpha1.Workflow
 
 	yaml.APIVersion = "argoproj.io/v1alpha1"
 	yaml.Kind = "Workflow"
-	yaml.Metadata.Name = wf_inputs.WorkName
-	yaml.Metadata.Namespace = wf_inputs.WorkNamespace
-	yaml.Metadata.Labels = map[string]string{
+	yaml.ObjectMeta.Name = wf_inputs.WorkName
+	yaml.ObjectMeta.Namespace = wf_inputs.WorkNamespace
+	yaml.ObjectMeta.Labels = map[string]string{
 		"cluster_id": wf_inputs.ClusterID,
 	}
-	var pram Parameter
+
+	var pram v1alpha1.Parameter
 	pram.Name = "adminModeNamespace"
-	pram.Value = wf_inputs.WorkNamespace
+	pram.Value = &wf_inputs.WorkNamespace
 	yaml.Spec.Arguments.Parameters = append(yaml.Spec.Arguments.Parameters, pram)
 
 	yaml.Spec.Entrypoint = "custom-chaos"
-	yaml.Spec.SecurityContext.RunAsNonRoot = true
-	yaml.Spec.SecurityContext.RunAsUser = 1000
+	*yaml.Spec.SecurityContext.RunAsNonRoot = true
+	*yaml.Spec.SecurityContext.RunAsUser = 1000
 
 	var (
-		custom_chaos        Template
-		install_experiments Template
-		engines             []Template
-		revert_chaos        Template
+		custom_chaos        v1alpha1.Template
+		install_experiments v1alpha1.Template
+		engines             []v1alpha1.Template
+		revert_chaos        v1alpha1.Template
 	)
 
 	custom_chaos.Name = "custom-chaos"
-	custom_chaos.Steps = append(custom_chaos.Steps, []Step{{Name: "install-chaos-experiments", Template: "install-chaos-experiments"}})
+	custom_chaos.Steps = append(custom_chaos.Steps, v1alpha1.ParallelSteps{Steps: []v1alpha1.WorkflowStep{
+		{
+			Name: "install-chaos-experiments",
+			Template: "install-chaos-experiments",
+		},
+	}})
 
 	install_experiments.Name = "install-chaos-experiments"
 	install_experiments.Container.Image = "lachlanevenson/k8s-kubectl"
@@ -187,7 +198,13 @@ func GenerateWorkflow(wf_inputs GenerateWorkflowInputs) ([]byte, error) {
 
 			wf_inputs.ExperimentName = &experiment
 
-			custom_chaos.Steps = append(custom_chaos.Steps, []Step{{Name: experiment, Template: experiment}})
+			custom_chaos.Steps = append(custom_chaos.Steps, v1alpha1.ParallelSteps{Steps: []v1alpha1.WorkflowStep{
+				{
+					Name: experiment,
+					Template: experiment,
+				},
+			}})
+
 			var file_type = "experiment"
 			wf_inputs.FileType = &file_type
 			yamlData, err := GetYamlData(wf_inputs)
@@ -195,13 +212,14 @@ func GenerateWorkflow(wf_inputs GenerateWorkflowInputs) ([]byte, error) {
 				log.Print(err)
 			}
 
-			install_experiments.Input.Artifacts = append(install_experiments.Input.Artifacts,
-				Artifact{
+			install_experiments.Inputs.Artifacts = append(install_experiments.Inputs.Artifacts,
+				v1alpha1.Artifact{
 					Name: experiment,
 					Path: "/tmp/" + experiment + ".yaml",
-
-					Raw: Raw{
-						Data: fmt.Sprint(yamlData.Data.GetYAMLData),
+					ArtifactLocation: v1alpha1.ArtifactLocation{
+						Raw: &v1alpha1.RawArtifact{
+							Data: fmt.Sprint(yamlData.Data.GetYAMLData),
+						},
 					},
 				})
 
@@ -217,15 +235,20 @@ func GenerateWorkflow(wf_inputs GenerateWorkflowInputs) ([]byte, error) {
 				log.Print(err)
 			}
 
-			var engine Template
+			var engine v1alpha1.Template
 			engine.Name = experiment
 			engine.Container.Args = append(engine.Container.Args, "-file=/tmp/chaosengine-"+experiment+".yaml")
 			engine.Container.Args = append(engine.Container.Args, "-saveName=/tmp/engine-name")
 			engine.Container.Image = "litmuschaos/litmus-checker:latest"
-			engine.Input.Artifacts = append(engine.Input.Artifacts, Artifact{
+			engine.Inputs.Artifacts = append(engine.Inputs.Artifacts, v1alpha1.Artifact{
 				Name: experiment,
 				Path: "/tmp/chaosengine-" + experiment + ".yaml",
-				Raw:  Raw{Data: fmt.Sprintln(yamlData.Data.GetYAMLData)},
+				ArtifactLocation: v1alpha1.ArtifactLocation{
+					Raw: &v1alpha1.RawArtifact{
+						Data: fmt.Sprint(yamlData.Data.GetYAMLData),
+					},
+				},
+
 			})
 
 			engines = append(engines, engine)
@@ -233,7 +256,13 @@ func GenerateWorkflow(wf_inputs GenerateWorkflowInputs) ([]byte, error) {
 	}
 
 	// Custom chaos
-	custom_chaos.Steps = append(custom_chaos.Steps, []Step{{Name: "revert-chaos", Template: "revert-chaos"}})
+	custom_chaos.Steps = append(custom_chaos.Steps, v1alpha1.ParallelSteps{Steps: []v1alpha1.WorkflowStep{
+		{
+			Name: "revert-chaos",
+			Template: "revert-chaos",
+		},
+	}})
+
 	yaml.Spec.Templates = append(yaml.Spec.Templates, custom_chaos)
 
 	// Install experiments
